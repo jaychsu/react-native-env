4a1c45bb33f59895b5efc289f9a7efd19d61a6a8
Jaych Su <hi@jaych.su>
2017-05-09 23:13:12 +0800

    Setup shared entry

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  Text,
+  View,
+} from 'react-native'
+
+export default function () {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>React Native Env</Text>
+    </View>
+  )
+}
