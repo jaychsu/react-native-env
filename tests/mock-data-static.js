bbcd53ae094cf451a10ab1c865a82a3cf6d1782a
Jaych Su <hi@jaych.su>
2017-05-13 14:34:01 +0800

    Add mock data for testing

---
+++ tests/mock-data-static.js
@@ -0,0 +1,695 @@
+import { mockImgSrc } from './mock-data'
+
+export default [
+  {
+    "g": "Google",
+    "o": 55,
+    "p": 0,
+    "s": mockImgSrc.local[3],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Autodesk",
+    "o": 69,
+    "p": 0,
+    "s": mockImgSrc.local[3],
+    "t": "Temple Run 2"
+  },
+  {
+    "g": "Google",
+    "o": 50,
+    "p": 2,
+    "s": mockImgSrc.local[6],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group B",
+    "o": 67,
+    "p": 1,
+    "s": mockImgSrc.local[6],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Facebook",
+    "o": 44,
+    "p": 4,
+    "s": mockImgSrc.local[6],
+    "t": "Temple Run 2"
+  },
+  {
+    "g": "Group B",
+    "o": 35,
+    "p": 3,
+    "s": mockImgSrc.local[3],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Group B",
+    "o": 15,
+    "p": 4,
+    "s": mockImgSrc.local[2],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "o": 28,
+    "p": 0,
+    "s": mockImgSrc.local[0],
+    "t": "Temple Run 2"
+  },
+  {
+    "g": "LinkedIn",
+    "o": 96,
+    "p": 0,
+    "s": mockImgSrc.local[1],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Group A",
+    "o": 36,
+    "p": 3,
+    "s": mockImgSrc.local[7],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Autodesk",
+    "o": 5,
+    "p": 4,
+    "s": mockImgSrc.local[7],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Group C",
+    "o": 52,
+    "p": 1,
+    "s": mockImgSrc.local[1],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Amazon",
+    "o": 29,
+    "p": 2,
+    "s": mockImgSrc.local[3],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group B",
+    "o": 48,
+    "p": 0,
+    "s": mockImgSrc.local[6],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Autodesk",
+    "o": 29,
+    "p": 3,
+    "s": mockImgSrc.local[1],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Group C",
+    "o": 10,
+    "p": 1,
+    "s": mockImgSrc.local[5],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group C",
+    "o": 16,
+    "p": 1,
+    "s": mockImgSrc.local[7],
+    "t": "BLUK"
+  },
+  {
+    "g": "LinkedIn",
+    "o": 85,
+    "p": 2,
+    "s": mockImgSrc.local[3],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Group A",
+    "o": 50,
+    "p": 0,
+    "s": mockImgSrc.local[1],
+    "t": "Temple Run 2"
+  },
+  {
+    "o": 85,
+    "p": 3,
+    "s": mockImgSrc.local[7],
+    "t": "BLUK"
+  },
+  {
+    "g": "Amazon",
+    "o": 6,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Facebook",
+    "o": 32,
+    "p": 3,
+    "s": mockImgSrc.local[2],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 20,
+    "p": 3,
+    "s": mockImgSrc.local[5],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group B",
+    "o": 80,
+    "p": 2,
+    "s": mockImgSrc.local[5],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Group C",
+    "o": 80,
+    "p": 3,
+    "s": mockImgSrc.local[0],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Facebook",
+    "o": 72,
+    "p": 4,
+    "s": mockImgSrc.local[6],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "o": 48,
+    "p": 2,
+    "s": mockImgSrc.local[1],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Group A",
+    "o": 15,
+    "p": 4,
+    "s": mockImgSrc.local[5],
+    "t": "Temple Run 2"
+  },
+  {
+    "g": "Facebook",
+    "o": 8,
+    "p": 1,
+    "s": mockImgSrc.local[6],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Google",
+    "o": 8,
+    "p": 4,
+    "s": mockImgSrc.local[5],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Group B",
+    "o": 14,
+    "p": 0,
+    "s": mockImgSrc.local[1],
+    "t": "BLUK"
+  },
+  {
+    "g": "Facebook",
+    "o": 35,
+    "p": 1,
+    "s": mockImgSrc.local[7],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "o": 40,
+    "p": 0,
+    "s": mockImgSrc.local[5],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Facebook",
+    "o": 34,
+    "p": 2,
+    "s": mockImgSrc.local[1],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "o": 62,
+    "p": 4,
+    "s": mockImgSrc.local[6],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Google",
+    "o": 48,
+    "p": 1,
+    "s": mockImgSrc.local[6],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Google",
+    "o": 42,
+    "p": 1,
+    "s": mockImgSrc.local[5],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 83,
+    "p": 3,
+    "s": mockImgSrc.local[0],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Google",
+    "o": 27,
+    "p": 4,
+    "s": mockImgSrc.local[3],
+    "t": "Temple Run 2"
+  },
+  {
+    "g": "Google",
+    "o": 7,
+    "p": 0,
+    "s": mockImgSrc.local[3],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Facebook",
+    "o": 40,
+    "p": 0,
+    "s": mockImgSrc.local[5],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 65,
+    "p": 1,
+    "s": mockImgSrc.local[5],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Facebook",
+    "o": 90,
+    "p": 4,
+    "s": mockImgSrc.local[4],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Group A",
+    "o": 88,
+    "p": 1,
+    "s": mockImgSrc.local[3],
+    "t": "BLUK"
+  },
+  {
+    "g": "Facebook",
+    "o": 73,
+    "p": 4,
+    "s": mockImgSrc.local[4],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Facebook",
+    "o": 4,
+    "p": 4,
+    "s": mockImgSrc.local[5],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Facebook",
+    "o": 32,
+    "p": 2,
+    "s": mockImgSrc.local[6],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group B",
+    "o": 41,
+    "p": 0,
+    "s": mockImgSrc.local[2],
+    "t": "BLUK"
+  },
+  {
+    "g": "Autodesk",
+    "o": 47,
+    "p": 4,
+    "s": mockImgSrc.local[0],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group A",
+    "o": 96,
+    "p": 0,
+    "s": mockImgSrc.local[5],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Amazon",
+    "o": 21,
+    "p": 3,
+    "s": mockImgSrc.local[3],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group C",
+    "o": 69,
+    "p": 2,
+    "s": mockImgSrc.local[2],
+    "t": "SketchBook"
+  },
+  {
+    "g": "LinkedIn",
+    "o": 10,
+    "p": 1,
+    "s": mockImgSrc.local[3],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group B",
+    "o": 71,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Group C",
+    "o": 66,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Group B",
+    "o": 73,
+    "p": 4,
+    "s": mockImgSrc.local[7],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "LinkedIn",
+    "o": 30,
+    "p": 1,
+    "s": mockImgSrc.local[3],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Autodesk",
+    "o": 13,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Autodesk",
+    "o": 21,
+    "p": 0,
+    "s": mockImgSrc.local[0],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Amazon",
+    "o": 3,
+    "p": 3,
+    "s": mockImgSrc.local[3],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Autodesk",
+    "o": 80,
+    "p": 1,
+    "s": mockImgSrc.local[6],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Google",
+    "o": 88,
+    "p": 1,
+    "s": mockImgSrc.local[3],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group C",
+    "o": 68,
+    "p": 4,
+    "s": mockImgSrc.local[1],
+    "t": "SketchBook"
+  },
+  {
+    "o": 53,
+    "p": 3,
+    "s": mockImgSrc.local[4],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Facebook",
+    "o": 1,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Autodesk",
+    "o": 33,
+    "p": 2,
+    "s": mockImgSrc.local[6],
+    "t": "Temple Run 2"
+  },
+  {
+    "g": "Group C",
+    "o": 99,
+    "p": 3,
+    "s": mockImgSrc.local[5],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 87,
+    "p": 0,
+    "s": mockImgSrc.local[5],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Autodesk",
+    "o": 97,
+    "p": 2,
+    "s": mockImgSrc.local[7],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group A",
+    "o": 80,
+    "p": 3,
+    "s": mockImgSrc.local[6],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Google",
+    "o": 19,
+    "p": 1,
+    "s": mockImgSrc.local[6],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Group A",
+    "o": 29,
+    "p": 3,
+    "s": mockImgSrc.local[7],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Autodesk",
+    "o": 96,
+    "p": 0,
+    "s": mockImgSrc.local[1],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "o": 14,
+    "p": 0,
+    "s": mockImgSrc.local[4],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Google",
+    "o": 65,
+    "p": 2,
+    "s": mockImgSrc.local[3],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 7,
+    "p": 0,
+    "s": mockImgSrc.local[2],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group C",
+    "o": 78,
+    "p": 2,
+    "s": mockImgSrc.local[5],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Group B",
+    "o": 60,
+    "p": 2,
+    "s": mockImgSrc.local[1],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Group A",
+    "o": 56,
+    "p": 3,
+    "s": mockImgSrc.local[0],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Facebook",
+    "o": 64,
+    "p": 4,
+    "s": mockImgSrc.local[4],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Autodesk",
+    "o": 86,
+    "p": 2,
+    "s": mockImgSrc.local[4],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Amazon",
+    "o": 86,
+    "p": 3,
+    "s": mockImgSrc.local[3],
+    "t": "SketchBook"
+  },
+  {
+    "g": "Group C",
+    "o": 43,
+    "p": 3,
+    "s": mockImgSrc.local[0],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "o": 10,
+    "p": 0,
+    "s": mockImgSrc.local[2],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Amazon",
+    "o": 40,
+    "p": 2,
+    "s": mockImgSrc.local[1],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "LinkedIn",
+    "o": 13,
+    "p": 0,
+    "s": mockImgSrc.local[2],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Group B",
+    "o": 73,
+    "p": 0,
+    "s": mockImgSrc.local[4],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Amazon",
+    "o": 28,
+    "p": 0,
+    "s": mockImgSrc.local[1],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 53,
+    "p": 0,
+    "s": mockImgSrc.local[6],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Amazon",
+    "o": 9,
+    "p": 1,
+    "s": mockImgSrc.local[7],
+    "t": "Chrome - web browser by Google"
+  },
+  {
+    "g": "Group B",
+    "o": 24,
+    "p": 4,
+    "s": mockImgSrc.local[0],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group A",
+    "o": 43,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "LinkedIn",
+    "o": 47,
+    "p": 2,
+    "s": mockImgSrc.local[4],
+    "t": "Boomerang from Instagram"
+  },
+  {
+    "g": "Amazon",
+    "o": 53,
+    "p": 1,
+    "s": mockImgSrc.local[2],
+    "t": "BLUK"
+  },
+  {
+    "o": 39,
+    "p": 3,
+    "s": mockImgSrc.local[6],
+    "t": "BLUK"
+  },
+  {
+    "g": "Autodesk",
+    "o": 58,
+    "p": 4,
+    "s": mockImgSrc.local[7],
+    "t": "BLUK"
+  },
+  {
+    "g": "Group B",
+    "o": 82,
+    "p": 4,
+    "s": mockImgSrc.local[0],
+    "t": "Cruise Ship Messenger"
+  },
+  {
+    "g": "Facebook",
+    "o": 77,
+    "p": 3,
+    "s": mockImgSrc.local[5],
+    "t": "SUPER MARIO RUN"
+  },
+  {
+    "g": "Facebook",
+    "o": 9,
+    "p": 4,
+    "s": mockImgSrc.local[4],
+    "t": "Nonstop Knight"
+  },
+  {
+    "g": "Amazon",
+    "o": 42,
+    "p": 4,
+    "s": mockImgSrc.local[6],
+    "t": "Temple Run 2"
+  }
+]
---
+++ tests/mock-data.js
@@ -0,0 +1,72 @@
+export const mockImgSrc = {
+  http: [
+    'http://iosicongallery.com/img/256/bluk-2017.png',
+    'http://iosicongallery.com/img/256/temple-run-2-2017.png',
+    'http://iosicongallery.com/img/256/super-mario-run-2016.png',
+    'http://iosicongallery.com/img/256/nonstop-knight-2016.png',
+    'http://iosicongallery.com/img/256/boomerang-from-instagram-2016.png',
+    'http://iosicongallery.com/img/256/cruise-ship-messenger-2016.png',
+    'http://iosicongallery.com/img/256/chrome-web-browser-by-google-2016.png',
+  ],
+  local: [
+    require('../assets/Facebook.jpg'),
+    require('../assets/iMovie.jpg'),
+    require('../assets/Instagram.jpg'),
+    require('../assets/LINE.jpg'),
+    require('../assets/LinkedIn.jpg'),
+    require('../assets/Messenger.jpg'),
+    require('../assets/Skype.jpg'),
+    require('../assets/YouTube.jpg'),
+  ],
+}
+
+const mockGroupName = [
+  'Group A',
+  'Group B',
+  'Group C',
+  'Facebook',
+  'Google',
+  'Autodesk',
+  'Amazon',
+  'LinkedIn',
+]
+
+const mockTitle = [
+  'SketchBook',
+  'Chrome - web browser by Google',
+  'Cruise Ship Messenger',
+  'Boomerang from Instagram',
+  'Nonstop Knight',
+  'SUPER MARIO RUN',
+  'Temple Run 2',
+  'BLUK',
+]
+
+function pickRandomItemFromArray(arr, isEmptyAllowed = false) {
+  const length = isEmptyAllowed
+    ? arr.length + 1
+    : arr.length
+  const index = getRandomNumber(length)
+  return arr[index]
+}
+
+function getRandomNumber(length) {
+  return Math.floor(Math.random() * length)
+}
+
+export default function getMockData(dataLength: number, srcType: 'http' | 'local') {
+  const imgSrc = (srcType === 'http')
+    ? mockImgSrc.http
+    : mockImgSrc.local
+  let result = []
+  for (let i = 0; i < dataLength; i++) {
+    result.push({
+      g: pickRandomItemFromArray(mockGroupName, true),
+      o: getRandomNumber(100),
+      p: getRandomNumber(5),
+      s: pickRandomItemFromArray(imgSrc),
+      t: pickRandomItemFromArray(mockTitle),
+    })
+  }
+  return result
+}
