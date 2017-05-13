625c2f08cffc85500b91a465a1f3742ad875afd6
Jaych Su <hi@jaych.su>
2017-05-13 14:34:23 +0800

    Add example to start development

--- app.js
+++ app.js
@@ -2,19 +2,61 @@ import React from 'react'
 import {
   Text,
   View,
+  Image,
 } from 'react-native'
 
-import Reseau from './components/react-native-reseau'
+import Reseau, {
+  formatUriSource,
+} from './components/react-native-reseau'
+import mockData from './tests/mock-data-static'
+
+const renderRow = (rowData, sectionID, rowID) => (
+  <View
+    style={{
+      width: 64,
+      height: 90,
+      marginBottom: 6,
+      marginLeft: 10,
+      marginRight: 10,
+    }}
+  >
+    <Image
+      source={formatUriSource(rowData.s)}
+      style={{
+        width: 64,
+        height: 64,
+        borderRadius: 10,
+      }}
+    />
+    <Text
+      style={{
+        paddingTop: 4,
+      }}
+      ellipsizeMode='tail'
+      numberOfLines={1}
+    >
+      {rowID}-{rowData.t}
+    </Text>
+  </View>
+)
 
 export default function () {
   return (
     <View
       style={{
         // TODO: Use a more graceful way to avoid overlapping content and head
-        paddingTop: 20
+        paddingTop: 20,
+        backgroundColor: '#ededed',
       }}
     >
-      <Reseau></Reseau>
+      <Reseau
+        data={mockData}
+        renderRow={renderRow}
+        pageSize={100}
+        contentContainerStyle={{
+          padding: 18,
+        }}
+      />
     </View>
   )
 }
